26d715ece120b105d0176ef0bc93ff70adc87c68
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-06-11 23:49:02 -0500

    First commit phonebook app

--- src/App.js
+++ src/App.js
@@ -1,54 +1,67 @@
+import { useState } from "react";
 import "./styles.css";
-import Course from "./components/Course";
 
-export default function App() {
-  const course = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ];
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newContact, setNewContact] = useState({ name: "", number: "" });
+  const [filter, setFilter] = useState("");
+  const inputHandler = (e) => {
+    setNewContact((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    let userSearch = persons.find((person) => person.name === newContact.name);
+    if (!userSearch) {
+      setPersons((prevState) => [...persons, { name: newContact.name }]);
+      setNewContact({ name: "", number: "" });
+    } else alert(`${newContact.name} is already added to phonebook`);
+  };
+
+  const onChangeSearch = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const fileredContacts = persons.filter((person) =>
+    person.name.includes(filter)
+  );
   return (
     <div>
-      <Course course={course} />
+      <h2>Phonebook</h2>
+
+      <div>
+        filter shown with <input onChange={onChangeSearch} value={filter} />
+      </div>
+
+      <h2>add a new</h2>
+      <form onSubmit={formSubmitHandler}>
+        <div>
+          name:{" "}
+          <input name="name" onChange={inputHandler} value={newContact.name} />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            name="number"
+            onChange={inputHandler}
+            value={newContact.number}
+          />
+        </div>
+        <div>debug name: {newContact.name}</div>
+        <div>debug number: {newContact.number}</div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {fileredContacts.map((person, key) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
   );
-}
+};
+
+export default App;
